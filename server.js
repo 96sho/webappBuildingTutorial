18e954db20294e011f0d28e39ea62cdaa40f3637
sho96 <shotaro962008@gmail.com>
2023-12-09 14:46:25 +0900

    ready to go

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const { readFileSync } = require("fs");
 
 const port = 8000;
-
 const app = express();
+app.use(express.json());
+
+let comments = [{user: "Someone", comment: "ユーザー名とコメントを入力して送信"}]
 
 app.get("/", (req, resp) => {
     resp.status(200).send(readFileSync("./index.html", { encoding: "utf-8"}));
 });
+app.post("/addComment", (req, resp) => {
+    comments[comments.length] = req.body;
+    resp.status(200).send();
+});
+app.get("/loadComments", (req, resp) => {
+    resp.status(200).send(JSON.stringify(comments));
+});
+
 
 
 app.listen(port);
